f73965d005df6f8e2ee3770d5f4cb61e2c372956
ragardner <26602401+ragardner@users.noreply.github.com>
2019-04-23 07:49:52 +0100

    Update tksheet_.py

--- tksheet/tksheet_.py
+++ tksheet/tksheet_.py
@@ -5329,4 +5329,48 @@ class Sheet(tk.Frame):
         self.xscroll.grid_forget()
 
 
+if __name__ == '__main__':
+    class demo(tk.Tk):
+        def __init__(self):
+            tk.Tk.__init__(self)
+            self.state("zoomed")
+            self.grid_columnconfigure(0, weight = 1)
+            self.grid_rowconfigure(0, weight = 1)
+            self.data = [[f"Row {r} Column {c}" for c in range(100)] for r in range(5000)]
+            self.sdem = Sheet(self,
+                              width = 1000,
+                              height = 700,
+                              align = "w",
+                              header_align = "center",
+                              row_index_align = "center",
+                              show = True,
+                              column_width = 180,
+                              row_index_width = 50,
+                              data_reference = self.data,
+                              headers=[f"Header {c}" for c in range(100)])
+            self.sdem.enable_bindings(("single",
+                                       "drag_select",
+                                       "col_drag_and_drop",
+                                       "row_drag_and_drop",
+                                       "col_select",
+                                       "row_select",
+                                       "col_width_resize",
+                                       "double_click_col_resize",
+                                       "row_width_resize",
+                                       "col_height_resize",
+                                       "arrowkeys",
+                                       "row_height_resize",
+                                       "double_click_row_resize"))
+            self.sdem.edit_bindings(True)
+            self.sdem.grid(row = 0, column = 0, sticky = "nswe")
+            self.sdem.highlight_cells(row = 0, column = 0, bg = "orange", fg = "blue")
+            self.sdem.highlight_cells(row = 0, bg = "orange", fg = "blue", canvas = "row_index")
+            self.sdem.highlight_cells(column = 0, bg = "orange", fg = "blue", canvas = "header")
+            self.mainloop()
+
+
+    app = demo()
+    app.mainloop()
+
+
 
