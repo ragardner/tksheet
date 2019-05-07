8da06bceb802f25d54dfe40182a09db7525fbf55
ragardner <26602401+ragardner@users.noreply.github.com>
2019-05-07 16:10:54 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,15 +2,16 @@ from distutils.core import setup
 setup(
   name = 'tksheet',
   packages = ['tksheet'],
-  version = '1.6',
+  version = '1.8',
   license='MIT',
   description = 'Tkinter table / sheet widget',
   author = 'ragardner',
   author_email = 'ragardner@protonmail.com',
   url = 'https://github.com/ragardner/tksheet',
-  download_url = 'https://github.com/ragardner/tksheet/archive/1.6.tar.gz',
+  download_url = 'https://github.com/ragardner/tksheet/archive/1.8.tar.gz',
   keywords = ['tkinter', 'table', 'widget'],
   install_requires=[],
+  include_package_data=True,
   classifiers=[
     'Development Status :: 5 - Production/Stable',
     'Intended Audience :: Developers',
