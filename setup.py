3b6dc925768b6734e017ab516558558f940b87bf
ragardner <26602401+ragardner@users.noreply.github.com>
2019-05-07 13:22:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'tksheet',
   packages = ['tksheet'],
-  version = '1.4',
+  version = '1.5',
   license='MIT',
   description = 'Tkinter table / sheet widget',
   author = 'ragardner',
   author_email = 'ragardner@protonmail.com',
   url = 'https://github.com/ragardner/tksheet',
-  download_url = 'https://github.com/ragardner/tksheet/archive/1.4.tar.gz',
+  download_url = 'https://github.com/ragardner/tksheet/archive/1.5.tar.gz',
   keywords = ['tkinter', 'table', 'widget'],
   install_requires=[],
   classifiers=[
