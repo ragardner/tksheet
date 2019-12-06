d9c986accc80cb7e1267c09ff4e864843fbae85a
ragardner <ragardner@protonmail.com>
2019-12-06 18:40:40 +0000

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
   name = 'tksheet',
-  py_modules = ['tksheet'],
-  version = '3.7',
+  packages = find_packages(),
+  version = '3.8',
   license='MIT',
   description = 'Tkinter table / sheet widget',
   author = 'ragardner',
   author_email = 'ragardner@protonmail.com',
   url = 'https://github.com/ragardner/tksheet',
-  download_url = 'https://github.com/ragardner/tksheet/archive/3.7.tar.gz',
+  download_url = 'https://github.com/ragardner/tksheet/archive/3.8.tar.gz',
   keywords = ['tkinter', 'table', 'widget'],
   install_requires=[],
   classifiers=[
