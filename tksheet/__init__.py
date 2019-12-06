5166a4a62539d9397c260ffc4eb5d90b42a14de2
ragardner <ragardner@protonmail.com>
2019-12-06 19:03:41 +0000

    Add files via upload

--- tksheet/__init__.py
+++ tksheet/__init__.py
@@ -1,7 +1,7 @@
-from tksheet._tksheet_column_headers import *
-from tksheet._tksheet_main_table import *
-from tksheet._tksheet_other_classes import *
-from tksheet._tksheet_row_index import *
-from tksheet._tksheet_top_left_rectangle import *
+from tksheet._tksheet_column_headers import ColumnHeaders
+from tksheet._tksheet_main_table import MainTable
+from tksheet._tksheet_other_classes import TableDropdown, TextEditor_, TextEditor
+from tksheet._tksheet_row_index import RowIndexes
+from tksheet._tksheet_top_left_rectangle import TopLeftRectangle
 from tksheet._tksheet_vars import *
-from tksheet._tksheet import *
+from tksheet._tksheet import Sheet
